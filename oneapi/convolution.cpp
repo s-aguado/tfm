11c3b5fc4eafbc86a116a796c46584b0f7a4629b
Sara Aguado <s.aguado@udc.es>
2021-05-02 01:35:48 +0200

    Finished 4D (batch) convolution in DPC++

--- oneapi/convolution.cpp
+++ oneapi/convolution.cpp
@@ -7,14 +7,14 @@
 /**
  * convolution.cpp
  * 
- * Implements a (basic 2D) convolution algorithm (TODO: in forward propagation mode). 
+ * Implements the direct convolution algorithm in forward propagation mode. 
  * Executes it in both, the CPU and the offload device, then compares 
  * the result. If the code executes on both CPU and the offload device, 
  * the name of the offload device and a success message are displayed.
  *
  * For comprehensive instructions regarding DPC++ Programming, go to
- * https://software.intel.com/en-us/oneapi-programming-guide and search based on
- * relevant terms noted in the comments.
+ * https://software.intel.com/en-us/oneapi-programming-guide and search based 
+ * on relevant terms noted in the comments.
  */
 
 #include <CL/sycl.hpp>
@@ -29,29 +29,69 @@ using namespace std;
 using namespace sycl;
 
 // Matrix size constants.
-constexpr int stride_h = 1;
-constexpr int stride_w = 1;
-constexpr int m_size = 1 << 6; // Must be a power of 2
-constexpr int H = m_size / 2;
-constexpr int W = m_size / 2;
-constexpr int R = m_size / 4;
-constexpr int S = m_size / 4;
-constexpr int P = (H - R) / stride_h + 1;
-constexpr int Q = (W - S) / stride_w + 1;
+constexpr int 
+  m_size = 1 << 6,            // Must be a power of 2
+  stride_h = 1,
+  stride_w = 1,
+  N = 4,                      // batch size *
+  C = 4,                      // input channels *
+  K = 4,                      // output channels/number of filters *
+  H = m_size / 2,             // image height *
+  W = m_size / 2,             // image width *
+  R = m_size / 4,             // filter height *
+  S = m_size / 4,             // filter width *
+  P = (H - R) / stride_h + 1, // output height
+  Q = (W - S) / stride_w + 1; // output width
+
+constexpr int // helper
+  hw = H*W,
+  rs = R*S,
+  pq = P*Q,
+  chw = C*H*W,
+  crs = C*R*S,
+  kpq = K*P*Q;
+
+/**
+ * Same initialization for device and host convolution
+ */
+void InitializeTensors(float(*x)[C][H][W], float(*f)[C][R][S], float(*y)[K][P][Q]) {
+  int n, c, k, h, w, r, s, p, q;
+
+  // Initialize each element of matrix a to 1
+  for (n = 0; n < N; n++)
+    for (c = 0; c < C; c++)
+      for (h = 0; h < H; h++)
+        for (w = 0; w < W; w++) x[n][c][h][w] = 1.0f;
+
+  // Initialize each column of b_host to the sequence 1,2,...,S
+  for (k = 0; k < K; k++)
+    for (c = 0; c < C; c++)
+      for (r = 0; r < R; r++)
+        for (s = 0; s < S; s++) f[k][c][r][s] = s + 1.0f;
+
+  // Initialize c_host to zero
+  for (n = 0; n < N; n++)
+    for (k = 0; k < K; k++)
+      for (p = 0; p < P; p++)
+        for (q = 0; q < Q; q++) y[n][k][p][q] = 0.0f;
+}
 
 /**
  * Perform convolution on host to verify results from device.
  */
-int VerifyResult(float (*c_back)[Q]);
+int VerifyResult(float(*c_back)[K][P][Q]);
 
+/**
+ * Perform convolution on device.
+ */
 int main() {
 
-  // Host memory buffer that device will write data back before destruction.
-  float(*c_back)[Q] = new float[P][Q];
+  // Tensors on device side
+  float(*a_back)[C][H][W] = new float[N][C][H][W];
+  float(*b_back)[C][R][S] = new float[K][C][R][S];
+  float(*c_back)[K][P][Q] = new float[N][K][P][Q];
 
-  // Initialize c_back to zero
-  for (int i = 0; i < P; i++)
-    for (int j = 0; j < Q; j++) c_back[i][j] = 0.0f;
+  InitializeTensors(a_back, b_back, c_back);
 
   try {
 
@@ -60,71 +100,60 @@ int main() {
     queue device_queue(default_selector{}, dpc_common::exception_handler);
     cout << "Device: " << device_queue.get_device().get_info<info::device::name>() << "\n";
 
-    // Create 2D buffers for matrices, buffer c is bound with host memory c_back
-    buffer<float, 2> a_buf(range(H, W));
-    buffer<float, 2> b_buf(range(R, S));
-    buffer c_buf(reinterpret_cast<float *>(c_back), range(P, Q));
+    // Create 4D buffers for tensors, buffer c is bound with host memory c_back
+    // Allocate DPC++ buffers for input and output memory objects
+    buffer a_buf(reinterpret_cast<float*>(a_back), range<1> { N*C*H*W });
+    buffer b_buf(reinterpret_cast<float*>(b_back), range<1> { K*C*R*S });
+    buffer c_buf(reinterpret_cast<float*>(c_back), range<1> { N*K*P*Q });
 
-    cout << "Multiplication size: c(" << P << "," << Q << ") = a(" << H << "," << W
-         << ") * b(" << R << "," << S << ")\n";
+    cout << "Multiplication size: c(" << N << "," << K << "," << P << "," << Q 
+         << ") = a(" << N << "," << C << "," << H << "," << W 
+         << ") * b(" << K << "," << C << "," << R << "," << S << ")\n";
 
-    // Using three command groups to illustrate execution order. The use of
-    // first two command groups for initializing matrices is not the most
-    // efficient way. It just demonstrates the implicit multiple command group
-    // execution ordering.
-
-    // Submit command group to queue to initialize matrix a
-    device_queue.submit([&](auto &h) {
-      accessor a(a_buf, h, write_only); // Get write only access to the buffer on the device
-      h.parallel_for(range(H, W), [=](auto index) {
-        a[index] = 1.0f; // Initialize each element of matrix a to 1
-      });
-    });
-
-    // Submit command group to queue to initialize matrix b
-    device_queue.submit([&](auto &h) {
-      accessor b(b_buf, h, write_only); // Get write only access to the buffer on the device
-      h.parallel_for(range(R, S), [=](auto index) {
-        b[index] = index[0] + 1.0f; // Initialize each column of b_host to the sequence 1,2,...,R
-      });
-    });
-
-    // Submit command group to queue to multiply matrices: c = a * b
-    device_queue.submit([&](auto &h) {
+    // Submit command group to queue to perform convolution: y = x * f
+    device_queue.submit([&](auto &context) {
 
       // Read from a and b, write to c
-      accessor a(a_buf, h, read_only);
-      accessor b(b_buf, h, read_only);
-      accessor c(c_buf, h, write_only);
+      accessor x(a_buf, context, read_only);
+      accessor f(b_buf, context, read_only);
+      accessor y(c_buf, context, read_write);
 
-      // Execute kernel.
-      h.parallel_for(range(P, Q), [=](auto index) {
+      // Execute kernel
+      context.parallel_for(range(N,K), [=](auto index) {
 
-        int p = index[0]; // Get global position in Y direction.
-        int q = index[1]; // Get global position in X direction.
+        int n = index[0];
+        int k = index[1];
 
-        // Compute the result of one element of c
-        for (int r = 0; r < R; r++) {
-          for (int s = 0; s < S; s++) {
+        auto y_ = &y[n * kpq + k * pq];
 
-            int h = p + r;
-            int w = q + s;
+        for (int c = 0; c < C; c++) {
 
-            c[p][q] += a[h][w] * b[r][s];
-          }
+          auto x_ = &x[n * chw + c * hw];
+          auto f_ = &f[k * crs + c * rs];
+
+          for (int p = 0; p < P; p++)
+            for (int q = 0; q < Q; q++)
+              for (int r = 0; r < R; r++)
+                for (int s = 0; s < S; s++) {
+
+                  int h = p + r;
+                  int w = q + s;
+
+                  y_[p*Q+q] += x_[h*W+w] * f_[r*S+s];
+                }
         }
       });
     });
   } catch (sycl::exception const &e) {
-    cout << "An exception is caught while multiplying matrices.\n";
+    cout << "An exception was caught during the convolution.\n";
     terminate();
-  }
+  } // c_back is updated when c_buf is destroyed upon exiting scope
 
   int result;
   cout << "Result of convolution using DPC++: ";
   result = VerifyResult(c_back);
+  
   delete[] c_back;
-
   return result;
 }
 
@@ -132,66 +161,67 @@ bool ValueSame(float a, float b) {
   return fabs(a - b) < numeric_limits<float>::epsilon();
 }
 
-int VerifyResult(float (*c_back)[Q]) {  
-  int i, j, k;
+bool Compare(float(*c_host)[K][P][Q], float(*c_back)[K][P][Q]) {
 
-  // 2D arrays on host side.
-  float(*a_host)[W] = new float[H][W];
-  float(*b_host)[S] = new float[R][S];
-  float(*c_host)[Q] = new float[P][Q];
+  // Compare host side results with the result buffer from device side: print
+  // mismatched data 4 times only.
+  
+  int printed_errors = 0;
 
-  // Initialize each element of matrix a to 1
-  for (i = 0; i < H; i++)
-    for (j = 0; j < W; j++) a_host[i][j] = 1.0f;
-
-  // Initialize each column of b_host to the sequence 1,2,...,R
-  for (i = 0; i < R; i++)
-    for (j = 0; j < S; j++) b_host[i][j] = i + 1.0f;
-
-  // Initialize c_host to zero
-  for (i = 0; i < P; i++)
-    for (j = 0; j < Q; j++) c_host[i][j] = 0.0f;
-
-  // Do the convolution
-  for (int p = 0; p < P; p++) { // output height
-    for (int q = 0; q < Q; q++) { // output width
-      for (int r = 0; r < R; r++) {
-        for (int s = 0; s < S; s++) {
-
-          int h = p + r;
-          int w = q + s;
-
-          c_host[p][q] += a_host[h][w] * b_host[r][s];
+  for (int n = 0; n < N; n++) {
+    for (int k = 0; k < K; k++) {
+      for (int p = 0; p < P; p++) {
+        for (int q = 0; q < Q; q++) {
+          if (!ValueSame(c_back[n][k][p][q], c_host[n][k][p][q])) {
+            cout << "\nFail - The result is incorrect for element: [" 
+                 << n << ", " << k << ", " << p << ", " << q << "], expected: " 
+                 << c_host[n][k][p][q] << ", but found: " << c_back[n][k][p][q];
+            if (++printed_errors == 4) return printed_errors;
+          }
         }
       }
     }
   }
 
-  // Compare host side results with the result buffer from device side: print
-  // mismatched data 5 times only.
-  int printed_errors = 0;
+  return printed_errors;
+}
 
-  for (i = 0; i < P; i++) {
-    for (j = 0; j < Q; j++) {
-      if (!ValueSame(c_back[i][j], c_host[i][j])) {
-        cout << "Fail - The result is incorrect for element: [" << i << ", "
-             << j << "], expected: " << c_host[i][j]
-             << ", but found: " << c_back[i][j] << "\n";
-        if (++printed_errors == 5) break;
-      }
-    }
-    if (printed_errors == 5) break;
-  }
+int VerifyResult(float(*c_back)[K][P][Q]) {  
+  int n, c, k, h, w, r, s, p, q;
+
+  // Tensors on host side
+  float(*a_host)[C][H][W] = new float[N][C][H][W];
+  float(*b_host)[C][R][S] = new float[K][C][R][S];
+  float(*c_host)[K][P][Q] = new float[N][K][P][Q];
+
+  InitializeTensors(a_host, b_host, c_host);
+
+  // Do the convolution
+  for (n = 0; n < N; n++)
+    for (k = 0; k < K; k++)
+      for (c = 0; c < C; c++)
+        for (p = 0; p < P; p++)
+          for (q = 0; q < Q; q++)
+            for (r = 0; r < R; r++)
+              for (s = 0; s < S; s++) {
+
+                h = p + r;
+                w = q + s;
+
+                c_host[n][k][p][q] += a_host[n][c][h][w] * b_host[k][c][r][s];
+              }
+
+  bool printed_errors = Compare(c_host, c_back);
 
   delete[] a_host;
   delete[] b_host;
   delete[] c_host;
 
-  if (printed_errors == 0) {
+  if (printed_errors) {
+    cout << "\nFail - The results mismatch!\n";
+    return -1;
+  } else {
     cout << "Success - The results are correct!\n";
     return 0;
-  } else {
-    cout << "Fail - The results mismatch!\n";
-    return -1;
   }
 }
