26cc89595e8dcf6b2c8225ec55f14f81172c33ae
Sara Aguado <s.aguado@udc.es>
2021-05-07 17:24:11 +0200

    Fixes GPU execution

--- onednn/utils.hpp
+++ onednn/utils.hpp
@@ -21,6 +21,32 @@
 #include "dnnl.hpp"
 #include "dnnl_debug.h"
 
+#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+  #include "dnnl_ocl.hpp"
+#elif DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL
+  #include "dnnl_sycl.hpp"
+#endif
+
+#if DNNL_CPU_THREADING_RUNTIME == DNNL_RUNTIME_OMP 
+  #ifdef _MSC_VER
+    #define PRAGMA_MACRo(x) __pragma(x)
+    #define PRAGMA_MACRO(x) PRAGMA_MACRo(x)
+  #else
+    #define PRAGMA_MACRo(x) _Pragma(#x)
+    #define PRAGMA_MACRO(x) PRAGMA_MACRo(x)
+  #endif
+
+  // MSVC doesn't support collapse clause in omp parallel
+  #if defined(_MSC_VER) && !defined(__clang__) && !defined(__INTEL_COMPILER)
+    #define collapse(x)
+  #endif
+
+  #define PRAGMA_OMP_PARALLEL_FOR_COLLAPSE(n) PRAGMA_MACRO(omp parallel for collapse(n))
+
+#else // DNNL_CPU_THREADING_RUNTIME != DNNL_RUNTIME_OMP
+  #define PRAGMA_OMP_PARALLEL_FOR_COLLAPSE(n)
+#endif
+
 // Exception class to indicate that the example uses a feature that is not
 // available on the current systems. It is not treated as an error then, but
 // just notifies a user.
@@ -83,14 +109,12 @@ inline int handle_errors(
     engine_kind, [&]() { example(engine_kind); });
 }
 
-// Validates that a GPU exists on the machine if engine::kind::gpu selected
+// Validates that at least one device of that kind exists on the machine
 dnnl::engine::kind validate_engine_kind(dnnl::engine::kind akind) {
-  if (akind == dnnl::engine::kind::gpu) {
-    if (dnnl::engine::get_count(dnnl::engine::kind::gpu) == 0) {
-      std::cout << "Application couldn't find GPU, please run with CPU "
-      "instead.\n";
-      exit(0);
-    }
+  if (dnnl::engine::get_count(akind) == 0) {
+    std::cout << "Application couldn't find any device for the selected engine."
+    << " Try with other engine kind instead.\n";
+    exit(0);
   }
   return akind;
 }
@@ -108,9 +132,9 @@ inline dnnl::engine::kind parse_engine_kind(int argc, char **argv) {
   if (engine_kind == "gpu")
     return validate_engine_kind(dnnl::engine::kind::gpu);
 
-  std::cout << "Inappropriate engine kind." << std::endl
+  std::cout << "Inappropriate engine kind.\n"
     << "Please indicate the engine kind in the first argument: " 
-    << argv[0] << " [cpu|gpu] [arguments..]" << std::endl;
+    << argv[0] << " [cpu|gpu] [arguments..]\n";
   
   exit(1);
 }
@@ -127,27 +151,57 @@ inline void read_from_dnnl_memory(void *handle, dnnl::memory &mem) {
 
   if (!handle) throw std::runtime_error("handle is nullptr.");
 
-  if (eng.get_kind() == dnnl::engine::kind::gpu) {
-  #if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
-    dnnl::stream s(eng);
-    cl_int ret = clEnqueueReadBuffer(
-      dnnl::ocl_interop::get_command_queue(s), 
-      dnnl::ocl_interop::get_mem_object(mem), 
-      CL_TRUE, 0, size, handle, 0, NULL, NULL
-    );
-    if (ret != CL_SUCCESS)
-      throw std::runtime_error("clEnqueueReadBuffer failed.");
-    return;
-  #else
-    assert(!"GPU selected but OpenCL not found");
+  #ifdef DNNL_WITH_SYCL
+    bool is_cpu_sycl = (DNNL_CPU_RUNTIME == DNNL_RUNTIME_SYCL
+      && eng.get_kind() == dnnl::engine::kind::cpu);
+    bool is_gpu_sycl = (DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL
+      && eng.get_kind() == dnnl::engine::kind::gpu);
+    if (is_cpu_sycl || is_gpu_sycl) {
+      auto mkind = dnnl::sycl_interop::get_memory_kind(mem);
+      if (mkind == dnnl::sycl_interop::memory_kind::buffer) {
+        auto buffer = dnnl::sycl_interop::get_buffer<uint8_t>(mem);
+        auto src = buffer.get_access<cl::sycl::access::mode::read>();
+        uint8_t *src_ptr = src.get_pointer();
+        if (!src_ptr)
+          throw std::runtime_error("get_pointer returned nullptr.");
+        for (size_t i = 0; i < size; ++i)
+          ((uint8_t *)handle)[i] = src_ptr[i];
+      } else {
+        assert(mkind == dnnl::sycl_interop::memory_kind::usm);
+        uint8_t *src_ptr = (uint8_t *)mem.get_data_handle();
+        if (!src_ptr)
+          throw std::runtime_error("get_data_handle returned nullptr.");
+        if (is_cpu_sycl) {
+          for (size_t i = 0; i < size; ++i)
+            ((uint8_t *)handle)[i] = src_ptr[i];
+        } else {
+          auto sycl_queue = dnnl::sycl_interop::get_queue(dnnl::stream(eng));
+          sycl_queue.memcpy(handle, src_ptr, size).wait();
+        }
+      }
+      return;
+    }
   #endif
-  }
+  
+  #if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+    if (eng.get_kind() == dnnl::engine::kind::gpu) {
+      dnnl::stream s(eng);
+      cl_int ret = clEnqueueReadBuffer(
+        dnnl::ocl_interop::get_command_queue(s), 
+        dnnl::ocl_interop::get_mem_object(mem), 
+        CL_TRUE, 0, size, handle, 0, NULL, NULL
+      );
+      if (ret != CL_SUCCESS)
+        throw std::runtime_error("clEnqueueReadBuffer failed.");
+      return;
+    }
+  #endif
 
   if (eng.get_kind() == dnnl::engine::kind::cpu) {
     uint8_t *src = static_cast<uint8_t *>(mem.get_data_handle());
     if (!src) throw std::runtime_error("get_data_handle returned nullptr.");
-    for (size_t i = 0; i < size; ++i)
-      ((uint8_t *)handle)[i] = src[i];
+      for (size_t i = 0; i < size; ++i)
+        ((uint8_t *)handle)[i] = src[i];
     return;
   }
 
@@ -161,26 +215,56 @@ inline void write_to_dnnl_memory(void *handle, dnnl::memory &mem) {
 
   if (!handle) throw std::runtime_error("handle is nullptr.");
 
-  if (eng.get_kind() == dnnl::engine::kind::gpu) {
-  #if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
-    dnnl::stream s(eng);
-    cl_int ret = clEnqueueWriteBuffer(
-      dnnl::ocl_interop::get_command_queue(s), 
-      dnnl::ocl_interop::get_mem_object(mem), 
-      CL_TRUE, 0, size, handle, 0, NULL, NULL
-    );
-    if (ret != CL_SUCCESS)
-      throw std::runtime_error("clEnqueueWriteBuffer failed.");
-    return;
-  #else 
-    assert(!"GPU selected but OpenCL not found");
+  #ifdef DNNL_WITH_SYCL
+    bool is_cpu_sycl = (DNNL_CPU_RUNTIME == DNNL_RUNTIME_SYCL
+      && eng.get_kind() == dnnl::engine::kind::cpu);
+    bool is_gpu_sycl = (DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL
+      && eng.get_kind() == dnnl::engine::kind::gpu);
+    if (is_cpu_sycl || is_gpu_sycl) {
+      auto mkind = dnnl::sycl_interop::get_memory_kind(mem);
+      if (mkind == dnnl::sycl_interop::memory_kind::buffer) {
+        auto buffer = dnnl::sycl_interop::get_buffer<uint8_t>(mem);
+        auto dst = buffer.get_access<cl::sycl::access::mode::write>();
+        uint8_t *dst_ptr = dst.get_pointer();
+        if (!dst_ptr)
+          throw std::runtime_error("get_pointer returned nullptr.");
+        for (size_t i = 0; i < size; ++i)
+          dst_ptr[i] = ((uint8_t *)handle)[i];
+      } else {
+        assert(mkind == dnnl::sycl_interop::memory_kind::usm);
+        uint8_t *dst_ptr = (uint8_t *)mem.get_data_handle();
+        if (!dst_ptr)
+          throw std::runtime_error("get_data_handle returned nullptr.");
+        if (is_cpu_sycl) {
+          for (size_t i = 0; i < size; ++i)
+            dst_ptr[i] = ((uint8_t *)handle)[i];
+        } else {
+          auto sycl_queue = dnnl::sycl_interop::get_queue(dnnl::stream(eng));
+          sycl_queue.memcpy(dst_ptr, handle, size).wait();
+        }
+      }
+      return;
+    }
   #endif
-  }
+
+  #if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+    if (eng.get_kind() == dnnl::engine::kind::gpu) {
+      dnnl::stream s(eng);
+      cl_int ret = clEnqueueWriteBuffer(
+        dnnl::ocl_interop::get_command_queue(s), 
+        dnnl::ocl_interop::get_mem_object(mem), 
+        CL_TRUE, 0, size, handle, 0, NULL, NULL
+      );
+      if (ret != CL_SUCCESS)
+        throw std::runtime_error("clEnqueueWriteBuffer failed.");
+      return;
+    }
+  #endif
 
   if (eng.get_kind() == dnnl::engine::kind::cpu) {
     uint8_t *dst = static_cast<uint8_t *>(mem.get_data_handle());
     if (!dst) throw std::runtime_error("get_data_handle returned nullptr.");
-    for (size_t i = 0; i < size; ++i) 
+    for (size_t i = 0; i < size; ++i)
       dst[i] = ((uint8_t *)handle)[i];
     return;
   }
